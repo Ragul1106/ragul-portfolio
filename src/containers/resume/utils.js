a53827836ec928b087ea9030b7e878dd5825e693
Ragul116 <ragulranjith1106@gmail.com>
2025-05-18 14:39:31 +0530

    initial commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Quality Trainee - ",
+        subTitle: "JBM auto LTD, Oragadam",
+        description:
+          " Quality assurance, process improvement, and testing to ensure product standards.",
+      },
+      {
+        title: "Team Lead - ",
+        subTitle: "APTIV COMPONENTS IND PVT LTD, chennai",
+        description:
+          " Co-ordinating teams to ensure quality and timely delivery.",
+      },
+      {
+        title: "Python FullStack Trainee",
+        subTitle: "VTS, Tenkasi",
+        description:
+          "Learning full-stack development with Python; involved in project management and team collaboration.",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary :",
+        subTitle: "S.M.H.Hr.Secondary School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary :",
+        subTitle: "S.M.H.Hr.Secondary School",
+        description:
+          "12th Grade in Vocational",
+      },
+      {
+        title: "College :",
+        subTitle: "MRK institute of technology",
+        description:
+          "Bachelor of Engineering in Electronics and Communication Engineering ",
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,58 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+        {
+           skillName: "REACT JS",
+           percentage: "70",
+         },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        // {
+        //   skillName: "JAVA",
+        //   percentage: "70",
+        // },
+        {
+          skillName: "PYTHON",
+          percentage: "65",
+        },
+        {
+          skillName: "RESTful Api",
+          percentage: "35",
+        },
+      ],
+    },
+    
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "MYSQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "45",
+        },
+        // {
+        //   skillName: "ORACLE",
+        //   percentage: "85",
+        // },
+      ],
+    },
+  ];
